2c0904cf193b0e0600afab170c596747262ae131
InnoFang <innofang@outlook.com>
2021-06-08 21:47:49 +0800

    test: test simple sparql parser

---
+++ data/p3.sql
@@ -0,0 +1,10 @@
+select ?x ?p1 ?p2 ?p3 where
+{
+	?x    <rdf:type>    <ub:UndergraduateStudent>.
+	?y    <ub:name> <Course1>.
+	?x    ?p1  ?y.
+	?z    <ub:teacherOf>    ?y.
+	?z    <ub:name> <FullProfessor1>.
+	?z    ?p2    ?w.
+	?w    ?p3    <Department0>.
+}
