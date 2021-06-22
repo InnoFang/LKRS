f33815b8abb7e06a685eddc0fae18777f4419422
InnoFang <innofang@outlook.com>
2021-06-22 23:14:04 +0800

    style: reanme 'tools' to 'main'

--- src/main/build.cpp
+++ src/main/build.cpp
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <chrono>
 #include "database/database.hpp"
 
 static const auto io_speed_up = [] {
@@ -18,7 +19,17 @@ int main (int argc, char* argv[]) {
     const std::string dbname = argv[1];
     const std::string datafile = argv[2];
     std::cout << dbname << " " << datafile << std::endl;
-    Database database(dbname, datafile);
-    database.load(dbname);
+    auto start_time = std::chrono::high_resolution_clock::now();
+
+    Database *db = new Database(dbname, datafile);
+
+    double used_time = std::chrono::duration_cast<std::chrono::duration<double>>(
+            std::chrono::high_resolution_clock::now() - start_time)
+            .count();
+
+    std::cout << "Used time: " << used_time << std::endl;
+
+    delete db;
+
     return 0;
 }
