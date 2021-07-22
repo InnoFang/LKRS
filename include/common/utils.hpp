ec5cbfd80567db6b1317753a9883e4c82df0910a
InnoFang <innofang@outlook.com>
2021-07-22 23:01:29 +0800

    pref: add namespace for utils.hpp

--- include/common/utils.hpp
+++ include/common/utils.hpp
@@ -7,17 +7,19 @@
 
 #include <vector>
 
-uint64_t encodePSO(const uint64_t& pid, const uint64_t& sid, const uint64_t& oid, const uint64_t& so_mask) {
-    uint64_t pso = (pid << (so_mask << 3) | (sid << (so_mask << 2) | oid;
-    return pso;
+namespace PSO {
+    uint64_t encode(const uint64_t& pid, const uint64_t& sid, const uint64_t& oid, const uint64_t& so_mask) {
+        uint64_t pso = (pid << (so_mask << 3) | (sid << (so_mask << 2) | oid;
+        return pso;
+    }
+
+    uint64_t decode(const uint64_t& pso, const uint64_t& p_mask, const uint64_t& so_mask) const {
+        uint64_t sid = (pso_ & so_mask) >> (so_mask << 2);
+        uint64_t pid = (pso_ &  p_mask) >> (so_mask << 3);
+        uint64_t oid = (pso_ & so_mask);
+
+        return std::vector<uint64_t> { sid, pid, oid };
+    };
 }
-
-uint64_t decodePSO(const uint64_t& pso, const uint64_t& p_mask, const uint64_t& so_mask) const {
-    uint64_t sid = (pso_ & so_mask) >> (so_mask << 2);
-    uint64_t pid = (pso_ &  p_mask) >> (so_mask << 3);
-    uint64_t oid = (pso_ & so_mask);
-
-    return std::vector<uint64_t> { sid, pid, oid };
-};
 
 #endif //RETRIEVE_SYSTEM_UTILS_HPP
