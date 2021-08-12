6480574e5816dd0fc3103a4031f87dde3be23b05
InnoFang <innofang@outlook.com>
2021-08-12 21:40:40 +0800

    feat: create triplet, easier to use than Triple

--- include/common/triple.hpp
+++ include/common/triple.hpp
@@ -7,6 +7,43 @@
 
 #include <string>
 #include <sstream>
+#include <utility>
+#include <vector>
+
+namespace gPSO {
+    using triplet = std::tuple<std::string, std::string, std::string>;
+    using pso_triplet = std::tuple<uint64_t , uint64_t, uint64_t>;
+
+    uint64_t encodePSO(uint64_t predicate, uint64_t subject, uint64_t object, int so_hex_len) {
+        uint64_t pso = predicate;
+        pso <<= (so_hex_len << 2);
+        pso |= subject;
+        pso <<= (so_hex_len << 2);
+        pso |= object;
+        return pso;
+    }
+
+    pso_triplet decodePSO(uint64_t pso, int so_hex_len) {
+        uint64_t so_mask = (1 << (so_hex_len << 2)) - 1;
+        uint64_t oid = (pso & so_mask);
+        pso >>= (so_hex_len << 2);
+        uint64_t sid = (pso & so_mask);
+        pso >>= (so_hex_len << 2);
+        uint64_t pid = pso;
+
+        return { pid, sid, oid };
+    };
+
+    pso_triplet decodePSO(uint64_t pso, uint64_t so_mask, int so_hex_len) {
+        uint64_t oid = (pso & so_mask);
+        pso >>= (so_hex_len << 2);
+        uint64_t sid = (pso & so_mask);
+        pso >>= (so_hex_len << 2);
+        uint64_t pid = pso;
+
+        return { pid, sid, oid };
+    };
+}
 
 class Triple {
 public:
