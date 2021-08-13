8de59de72c8d96fae097e60450e32bc46200bd10
InnoFang <innofang@outlook.com>
2021-08-13 22:00:42 +0800

    pref: remove io_speed_up function

--- src/cmd/psoBuild.cpp
+++ src/cmd/psoBuild.cpp
@@ -7,38 +7,26 @@
 #include <chrono>
 #include "database/database.hpp"
 
-//static const auto io_speed_up = [] {
-//    std::istringstream::sync_with_stdio(false);
-//    std::ostringstream::sync_with_stdio(false);
-//    std::ifstream::sync_with_stdio(false);
-//    std::ofstream::sync_with_stdio(false);
-//    std::ios::sync_with_stdio(false);
-//    std::cin.tie(nullptr);
-//    std::cout.tie(nullptr);
-//    std::fstream::sync_with_stdio(false);
-//    return 0;
-//}();
-
 int main (int argc, char* argv[]) {
-    const std::string dbname = argv[1];
-    const std::string datafile = argv[2];
-    std::cout << dbname << " " << datafile << std::endl;
-
-    Database *db = nullptr;
-    try {
-        auto start_time = std::chrono::high_resolution_clock::now();
-
-        db = new Database(dbname);
-        db->create(datafile);
-
-        double used_time = std::chrono::duration_cast<std::chrono::duration<double>>(
-                std::chrono::high_resolution_clock::now() - start_time)
-                .count();
-        std::cout << "Used time: " << used_time << std::endl;
-    } catch(std::exception &e) {
-        delete db;
+    if (argc != 3) {
+        std::cerr << "./psoBuild <db_name> <raw_rdf_file_path>" << std::endl;
         return 0;
     }
-    delete db;
+
+    std::string dbname = argv[1];
+    std::string datafile = argv[2];
+    std::cout << dbname << " " << datafile << std::endl;
+
+    Database db(dbname);
+
+    auto start_time = std::chrono::high_resolution_clock::now();
+
+    db.create(datafile);
+
+    double used_time = std::chrono::duration_cast<std::chrono::duration<double>>(
+            std::chrono::high_resolution_clock::now() - start_time)
+            .count();
+
+    std::cout << "Used time: " << used_time << std::endl;
     return 0;
 }
