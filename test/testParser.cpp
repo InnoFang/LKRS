0aa4711708201e4698ba8d56b7934061128824ad
InnoFang <innofang@outlook.com>
2021-09-02 11:09:08 +0800

    test: add test for sparql_parser

--- test/testParser.cpp
+++ test/testParser.cpp
@@ -1,44 +1,42 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include "parser/SPARQLParser.hpp"
+#include <spdlog/spdlog.h>
+#include <gtest/gtest.h>
+#include "parser/sparql_parser.hpp"
 
-void testSPARQLParser(std::string &sparql) {
-    SPARQLParser parser(sparql);
-    std::cout << "[parser] variables:" << std::endl;
-    for (const auto &item : parser.getQueryVariables()) {
-        std::cout << item << std::endl;
-    }
-
-    std::cout << "[parser] triples_:" << std::endl;
-    for (const Triple &triple : parser.getQueryTriples()) {
-        std::cout << triple.s << " " << triple.p << " " << triple.o << " " << std::endl;
-    }
-}
-
-void test1() {
+TEST(SparqlParserTest, IndistinctSparql) {
     std::string sparql = "select ?x ?p where { ?x ?p <FullProfessor0>. }";
-    testSPARQLParser(sparql);
+    SparqlParser sparqlParser(sparql);
+
+    auto distinct = sparqlParser.isDistinct();
+    EXPECT_FALSE(distinct);
+
+    auto query_variables = sparqlParser.getQueryVariables();
+    auto expect_variables = std::vector<std::string> {"?x", "?p"};
+    EXPECT_EQ(expect_variables, query_variables);
 }
 
-void test2() {
-    std::string filename = R"(../data/p3.sql)";
-    std::ifstream infile(filename, std::ios::in);
-    std::ostringstream buf;
-    char ch;
-    if (infile.is_open()) {
-        while (buf && infile.get(ch)) {
-            buf.put(ch);
-        }
-        std::string sparql = buf.str();
-        testSPARQLParser(sparql);
-    } else {
-        std::cout << "cannot open file: " << std::endl;
-    }
-    infile.close();
-}
 
-int main() {
-    test2();
-    return 0;
+//void test2() {
+//    std::string filename = R"(../data/p3.sql)";
+//    std::ifstream infile(filename, std::ios::in);
+//    std::ostringstream buf;
+//    char ch;
+//    if (infile.is_open()) {
+//        while (buf && infile.get(ch)) {
+//            buf.put(ch);
+//        }
+//        std::string sparql = buf.str();
+//        testSPARQLParser(sparql);
+//    } else {
+//        std::cout << "cannot open file: " << std::endl;
+//    }
+//    infile.close();
+//}
+
+int main(int argc, char** argv) {
+    printf("Running main() from %s\n", __FILE__);
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
