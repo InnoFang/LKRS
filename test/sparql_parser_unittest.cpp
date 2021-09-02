73c0107a38c896fdff3a87c4b445fbebf8014cc9
InnoFang <innofang@outlook.com>
2021-09-02 11:29:59 +0800

    test: add test case for sparql_parser

--- test/sparql_parser_unittest.cpp
+++ test/sparql_parser_unittest.cpp
@@ -1,4 +1,3 @@
-#include <iostream>
 #include <fstream>
 #include <string>
 #include <spdlog/spdlog.h>
@@ -27,6 +26,50 @@ TEST(SparqlParserTest, DistinctSparql) {
     auto query_variables = sparqlParser.getQueryVariables();
     auto expect_variables = std::vector<std::string> {"?x", "?p"};
     EXPECT_EQ(expect_variables, query_variables);
+}
+
+std::string readSPARQLFromFile(const std::string& filepath) {
+    std::ifstream infile(filepath, std::ios::in);
+    std::ifstream::sync_with_stdio(false);
+    std::ostringstream buf;
+    std::string sparql;
+    char ch;
+    if (infile.is_open()) {
+        while (buf && infile.get(ch)) {
+            buf.put(ch);
+        }
+        sparql = buf.str();
+    } else {
+        std::cerr << "cannot open file: " << filepath << std::endl;
+    }
+    infile.close();
+    return sparql;
+}
+
+TEST(SparqlParserTest, ParseSqlFile) {
+    std::string filename = R"(../../data/lubm/lubm_q1.sql)";
+    std::string sparql = readSPARQLFromFile(filename);
+    std::string expect_sparql = "select distinct ?x where\n"
+                                "{\n"
+                                "?x\t<rdf:type>\t<ub:GraduateStudent>.\n"
+                                "?y\t<rdf:type>\t<ub:University>.\n"
+                                "?z\t<rdf:type>\t<ub:Department>.\n"
+                                "?x\t<ub:memberOf>\t?z.\n"
+                                "?z\t<ub:subOrganizationOf>\t?y.\n"
+                                "?x\t<ub:undergraduateDegreeFrom>\t?y.\n"
+                                "}\n";
+    EXPECT_EQ(expect_sparql, sparql);
+    SparqlParser sparqlParser(sparql);
+
+    auto distinct = sparqlParser.isDistinct();
+    EXPECT_TRUE(distinct);
+
+    auto query_variables = sparqlParser.getQueryVariables();
+    auto expect_variables = std::vector<std::string> {"?x"};
+    EXPECT_EQ(expect_variables, query_variables);
+
+    auto query_triples_size = sparqlParser.getQueryTriples().size();
+    EXPECT_EQ(6, query_triples_size);
 }
 
 int main(int argc, char** argv) {
