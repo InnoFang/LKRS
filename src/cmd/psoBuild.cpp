0640ca67c30596ace804df71b68ba8a1b8c885a9
InnoFang <innofang@outlook.com>
2021-10-14 11:34:49 +0800

    refactor: use DatabaseBuilder instead of Database

--- src/cmd/psoBuild.cpp
+++ src/cmd/psoBuild.cpp
@@ -1,11 +1,15 @@
-//
-// Created by InnoFang on 2021/6/19.
-//
+/*
+ * @FileName   : psoBuild.cpp
+ * @CreateAt   : 2021/6/19
+ * @Author     : Inno Fang
+ * @Email      : innofang@yeah.net
+ * @Description: command-line tool for handle database creation.
+ */
 
 #include <iostream>
 #include <string>
 #include <chrono>
-#include "database/database.hpp"
+#include "database/database_builder.hpp"
 
 int main (int argc, char* argv[]) {
     if (argc != 3) {
@@ -17,16 +21,13 @@ int main (int argc, char* argv[]) {
     std::string datafile = argv[2];
     std::cout << dbname << " " << datafile << std::endl;
 
-    Database db(dbname);
-
+    DatabaseBuilder db;
     auto start_time = std::chrono::high_resolution_clock::now();
 
-    db.create(datafile);
+    db.create(dbname, datafile);
 
     auto stop_time = std::chrono::high_resolution_clock::now();
-
     std::chrono::duration<double, std::milli> used_time = stop_time - start_time;
-
     std::cout << "Used time: " << used_time.count() << " ms." << std::endl;
     return 0;
 }
