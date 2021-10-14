7bfc3ae35bced54fd4ef7b0dfaaf3056fe78f9c5
InnoFang <innofang@outlook.com>
2021-10-14 16:24:37 +0800

    feat: update ResultSet

--- include/common/result_set.hpp
+++ include/common/result_set.hpp
@@ -17,33 +17,75 @@ class ResultSet {
 public:
     using key_type = Key;
     using value_type = Value;
-
 private:
     using item_type = std::unordered_map<key_type, value_type>;
 
 public:
-    ResultSet(): result_(0) {}
+
+    ResultSet() = default;
     explicit ResultSet(const size_t &size): result_(size) {}
-    ResultSet(const size_t &size, item_type item): result_(size, item) {};
-    ResultSet(const ResultSet<key_type, value_type> &other): result_(other) {};
-    ResultSet(ResultSet<key_type, value_type> &&other) noexcept : result_(other) {};
+    ResultSet(const size_t &size, item_type item): result_(size, item) {}
+    ResultSet(const ResultSet<key_type, value_type> &other): result_(other.result_) {}
+    ResultSet(ResultSet<key_type, value_type> &&other) noexcept : result_(std::move(other.result_)) {}
     ~ResultSet() = default;
 
-    typename std::vector<item_type>::iterator begin() { return result_.begin(); }
-    typename std::vector<item_type>::iterator begin() const { return result_.begin(); }
-    typename std::vector<item_type>::iterator end() { return result_.end(); }
-    typename std::vector<item_type>::iterator end() const { return result_.end(); }
+    decltype(auto) begin() { return result_.begin(); }
+    decltype(auto) begin() const { return result_.begin(); }
+    decltype(auto) end() { return result_.end(); }
+    decltype(auto) end() const { return result_.end(); }
+
+    decltype(auto) reserve(const std::size_t &size) {
+        return result_.resize(size);
+    }
 
     decltype(auto) emplace_back(const item_type &item) {
-        result_.emplace_back(item);
+        return result_.emplace_back(item);
     }
 
     decltype(auto) push_back(const item_type &item) {
-        result_.push_back(item);
+        return result_.push_back(item);
     }
 
     decltype(auto) erase(const item_type &item) {
         return result_.erase(item);
+    }
+
+    decltype(auto) clear() {
+        return result_.clear();
+    }
+
+    std::size_t size() {
+        return result_.size();
+    }
+
+    bool empty() {
+        return result_.empty();
+    }
+
+    item_type &operator[](size_t index) {
+        return result_.at[index];
+    }
+
+    ResultSet<key_type, value_type> operator==(const ResultSet<key_type, value_type> &other) {
+        return this->result_ == other.result_;
+    }
+
+    ResultSet<key_type, value_type> operator!=(const ResultSet<key_type, value_type> &other) {
+        return this->result_ != other.result_;
+    }
+
+    ResultSet<key_type, value_type> &operator=(const ResultSet<key_type, value_type> &other) {
+        if (this != other) {
+            result_ = other.result_;
+        }
+        return *this;
+    }
+
+    ResultSet<key_type, value_type> &operator=(ResultSet<key_type, value_type> &&other) noexcept {
+        if (this != &other) {
+            result_ = std::move(other.result_);
+        }
+        return *this;
     }
 
 private:
