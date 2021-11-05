096345586af319bfb8b8f5c96951a06d8b5fb8d5
InnoFang <innofang@outlook.com>
2021-11-05 22:41:09 +0800

    feat: add time counting macro

---
+++ include/common/utils.hpp
@@ -0,0 +1,20 @@
+﻿/*
+ * @FileName   : macros.hpp 
+ * @CreateAt   : 2021/11/4
+ * @Author     : Inno Fang
+ * @Email      : innofang@yeah.net
+ * @Description: 
+ */
+
+#ifndef RETRIEVE_SYSTEM_UTILS_HPP
+#define RETRIEVE_SYSTEM_UTILS_HPP
+
+#define TIMEIT( CODE, RECORD ) do { \
+            auto start_time = std::chrono::high_resolution_clock::now(); \
+            CODE;  \
+            auto stop_time = std::chrono::high_resolution_clock::now(); \
+            std::chrono::duration<double, std::milli> used_time = stop_time - start_time; \
+            (RECORD) = used_time.count(); \
+        } while(0);
+
+#endif //RETRIEVE_SYSTEM_UTILS_HPP
