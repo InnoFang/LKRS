410fbcd635d82d85e5594c4748247ec16fe939f1
InnoFang <innofang@outlook.com>
2021-11-06 16:35:09 +0800

    feat: update timing function, it's very useful

--- include/common/utils.hpp
+++ include/common/utils.hpp
@@ -3,12 +3,13 @@
  * @CreateAt   : 2021/11/4
  * @Author     : Inno Fang
  * @Email      : innofang@yeah.net
- * @Description: 
+ * @Description: utils collections
  */
 
 #ifndef RETRIEVE_SYSTEM_UTILS_HPP
 #define RETRIEVE_SYSTEM_UTILS_HPP
 
+// DEPRECATION !!
 #define TIMEIT( CODE, RECORD ) do { \
             auto start_time = std::chrono::high_resolution_clock::now(); \
             CODE;  \
@@ -16,5 +17,15 @@
             std::chrono::duration<double, std::milli> used_time = stop_time - start_time; \
             (RECORD) = used_time.count(); \
         } while(0);
+
+/* RECOMMEND!! timing function */
+template<typename Function, typename... Types>
+decltype(auto) timeit(Function &&function, Types &&...args) {
+    auto start_time = std::chrono::high_resolution_clock::now();
+    auto ret = function(args...);
+    auto stop_time = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double, std::milli> diff = stop_time - start_time; \
+    return std::make_tuple(std::move(ret), diff.count());
+}
 
 #endif //RETRIEVE_SYSTEM_UTILS_HPP
