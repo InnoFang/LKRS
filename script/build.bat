9b2cc5ac078cd3b35441c7051d1a38ceb0c3300c
InnoFang <innofang@outlook.com>
2021-11-15 22:08:41 +0800

    feat: create build.bat

---
+++ script/build.bat
@@ -0,0 +1,22 @@
+﻿@echo off & CHCP 65001
+@title build project
+
+cd ..
+
+echo "create build"
+if not exist build (
+    mkdir build
+)
+
+echo "create bin"
+if not exist bin (
+    mkdir bin
+)
+
+cd build
+
+conan install .. -s build_type=Debug
+cmake ..
+cmake --build . --config Release
+
+pause
