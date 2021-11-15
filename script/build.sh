4ee46f674e0d4064028fcb442982daf7e849c76e
InnoFang <innofang@outlook.com>
2021-11-15 21:50:07 +0800

    feat: create build.sh

---
+++ script/build.sh
@@ -0,0 +1,12 @@
+﻿#!/usr/bin/env bash
+
+test -e ../build || mkdir -p ../build
+echo "mkdir build"
+
+test -e ../bin || mkdir -p ../bin
+echo "mkdir bin"
+
+cd ../build
+conan install ..
+cmake .. -DCMAKE_BUILD_TYPE=Release
+cmake --build .
