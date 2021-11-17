b264ee6bab0004249e0490070868286ef4f63c45
InnoFang <innofang@outlook.com>
2021-11-18 00:23:18 +0800

    feat: update load method

--- src/cmd/psoHttp.cpp
+++ src/cmd/psoHttp.cpp
@@ -66,74 +66,116 @@ bool execute_insert(std::string &sparql) {
 }
 
 void load(const httplib::Request &req, httplib::Response &res) {
+    spdlog::info("Catch Load Request.");
     res.set_header("Access-Control-Allow-Origin", "*");
 //    auto predicates = db->getPredicateStatistics();
-    std::unordered_set<uint32_t> node_set;
-    std::set<std::pair<uint32_t, uint32_t>> edge_set;
+    std::unordered_map<uint32_t, nlohmann::json> node_map;
+    std::set<std::pair<std::string, std::string>> edge_set;
     std::vector<nlohmann::json> nodes;
     std::vector<nlohmann::json> edges;
 
-    std::vector<uint32_t> predicates {1, 2, 3, 4, 6, 7, 18, 19, 20, 21};
-//    for (int pid = 1; pid < predicates.size(); ++pid) {
-    for (auto &pid: predicates) {
-        auto so = db->getSOByP(pid);
-        int num = 20;
+                                    //0  1  2  3  4  5   6   7   8   9
+    std::vector<uint32_t> pid_list {1, 2, 3, 4, 6, 7, 18, 19, 20, 21};
+//    std::vector<std::string> categories{"类别", "子类别", "品牌", "品名", "价格", "评论", "处理器型号", "方面", "观点"};
+    std::vector<std::string> categories{"Category", "sub-Category", "Brand", "Product", "Price", "Comment", "CPU", "Aspect", "Opinion"};
+    std::unordered_map<uint32_t, std::pair<uint32_t, uint32_t>> categories_map{
+            // {pid, {source_category_idx, target_category_idx}}
+            {1,  {0, 1}},
+            {2,  {1, 2}},
+            {3,  {2, 3}},
+            {4,  {2, 4}},
+            {6,  {2, 5}},
+            {7,  {2, 6}},
+            {18, {5, 7}},
+            {19, {5, 8}},
+            {20, {7, 8}},
+    };
+
+    for (auto &pid : pid_list) {
+        std::string predicate = db->getPredicateById(pid);
+        auto so = db->getS2OByP(pid);
+        size_t source_cate_idx = categories_map[pid].first;
+        size_t target_cate_idx = categories_map[pid].second;
+        int num = 50;
         for (const auto &item : so) {
             if (pid > 7 && num != 0) num--;
             else if (pid > 7) break;
-            std::string sid_str = std::to_string(item.first);
-            std::string oid_str = std::to_string(item.second);
-            nlohmann::json s = {
-                    {"id", sid_str},
-                    {"name", db->getEntityById(item.first)}
-            };
-            nlohmann::json o = {
-                    {"id", oid_str},
-                    {"name", db->getEntityById(item.second)}
-            };
-            nlohmann::json p = {
-                    {"source", sid_str},
-                    {"target", oid_str},
-                    {"label", db->getPredicateById(pid)}
-            };
-            if (!node_set.count(item.first)) {
-                node_set.insert(item.first);
+
+            uint32_t sid = item.first;
+            uint32_t oid = item.second;
+            std::string sid_str = std::to_string(sid);
+            std::string oid_str = std::to_string(oid);
+
+            if (!node_map.count(sid)) {
+                nlohmann::json s = {
+                        {"id", sid_str},
+                        {"name", db->getEntityById(sid)},
+                        {"category", source_cate_idx}
+                };
+                node_map[sid] = s;
                 nodes.emplace_back(s);
             }
-            if (!node_set.count(item.second)) {
-                node_set.insert(item.second);
+
+            if (!node_map.count(oid)) {
+                nlohmann::json o = {
+                        {"id", oid_str},
+                        {"name", db->getEntityById(oid)},
+                        {"category", target_cate_idx}
+                };
+                node_map[oid] = o;
                 nodes.emplace_back(o);
             }
 
-            auto pair = std::make_pair(item.first, item.second);
+            auto pair = std::make_pair(node_map[sid]["id"], node_map[oid]["id"]);
             if (!edge_set.count(pair)) {
+                nlohmann::json p = {
+                        {"source", node_map[sid]["id"]},
+                        {"target", node_map[oid]["id"]},
+                        {"label", predicate}
+                };
                 edge_set.insert(pair);
                 edges.emplace_back(p);
             }
         }
     }
 
+    std::vector<std::unordered_map<std::string, std::string>> categories_data;
+    categories_data.reserve(categories.size());
+    for (std::string category : categories) {
+        categories_data.push_back({{"name", category}});
+    }
+
     nlohmann::json j;
     j["nodes"] = nodes;
     j["edges"] = edges;
+    j["categories"] = categories_data;
     res.set_content(j.dump(2), "text/plain");
 }
 
 void query(const httplib::Request &req, httplib::Response &res) {
+    spdlog::info("Catch Query Request.");
+    res.set_header("Access-Control-Allow-Origin", "*");
     if (!req.has_param("sparql")) {
         return;
     }
+
     std::string sparql = req.get_param_value("sparql");
+    spdlog::info("Receive SPARQL: {}", sparql);
+
     nlohmann::json json(execute_query(sparql));
     res.set_content(json.dump(2), "text/plain");
 }
 
 void insert(const httplib::Request &req, httplib::Response &res) {
+    spdlog::info("Catch Insert Request.");
+    res.set_header("Access-Control-Allow-Origin", "*");
     if (!req.has_param("sparql")) {
         return;
     }
 
     std::string sparql = req.get_param_value("sparql");
+    spdlog::info("Receive SPARQL: {}", sparql);
+
     bool status = execute_insert(sparql);
     if (status) {
         res.set_content("successfully", "text/plain");
@@ -164,8 +206,8 @@ int main(int argc, char **argv) {
     }
 
     std::string host = vm["host"].as<std::string>();
-    spdlog::info("{}", host);
     int port = vm["port"].as<int>();
+    spdlog::info("Running at:  http://{}:{}", host, port);
     std::string db_name = vm["db_name"].as<std::string>();
 
     db = inno::DatabaseBuilder::Load(db_name);
