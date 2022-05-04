3aace20d5479bdb7d279e7502652e0ebe7305ef6
InnoFang <innofang@outlook.com>
2022-05-04 16:54:12 +0800

    chore: upload legacy files

---
+++ .legacy/common/triplet.hpp
@@ -0,0 +1,37 @@
+//
+// Created by InnoFang on 2021/6/7.
+//
+
+#ifndef RS_INCLUDE_COMMON_TRIPLET_HPP_
+#define RS_INCLUDE_COMMON_TRIPLET_HPP_
+
+#include <string>
+#include <sstream>
+#include <utility>
+#include <vector>
+#include <tuple>
+
+namespace gPSO {
+
+/* S, P, O */
+using triplet = std::tuple<std::string, std::string, std::string>;
+/* P, S, O */
+using pso_triplet = std::tuple<uint64_t , uint64_t, uint64_t>;
+
+extern uint64_t encodePSO(uint64_t predicate, uint64_t subject, uint64_t object, int so_hex_len);
+
+extern pso_triplet decodePSO(uint64_t pso, int so_hex_len);
+
+extern pso_triplet decodePSO(uint64_t pso, uint64_t so_mask, int so_hex_len);
+
+struct triplet_hash {
+    size_t operator() (const gPSO::triplet& t) const {
+        std::string s, p, o;
+        std::tie(s, p, o) = t;
+        return std::hash<std::string>()(s + p + o);
+    }
+};
+
+} // namespace gPSO
+
+#endif //RS_INCLUDE_COMMON_TRIPLET_HPP_
